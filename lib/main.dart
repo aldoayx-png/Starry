a32b5f4da31917a6f9c4f901721b7b3532d18380
aldoayx-png <aldoayx@gmail.com>
2026-04-18 02:43:33 -0600

    jjj

--- lib/main.dart
+++ lib/main.dart
@@ -450,18 +450,6 @@ class _DreamJournalHomeState extends State<DreamJournalHome>
                                 _dreams[idx] = result['dream'];
                               }
                             });
-                            // Refrescar lista después de editar
-                            Future.delayed(
-                              const Duration(milliseconds: 300),
-                              () {
-                                if (mounted) {
-                                  debugPrint(
-                                    'Refrescando lista después de editar...',
-                                  );
-                                  _fetchDreams();
-                                }
-                              },
-                            );
                           }
                         }
                       },
